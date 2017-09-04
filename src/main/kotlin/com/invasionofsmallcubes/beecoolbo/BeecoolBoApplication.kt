2fdb8b71eb2c2baacc9792f04a8e111510da6e2f
Emanuele Ianni <dierre@gmail.com>
2017-09-04 20:22:39 +0200

    initial commit - db not working

---
+++ src/main/kotlin/com/invasionofsmallcubes/beecoolbo/BeecoolBoApplication.kt
@@ -0,0 +1,11 @@
+package com.invasionofsmallcubes.beecoolbo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BeecoolBoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BeecoolBoApplication::class.java, *args)
+}
