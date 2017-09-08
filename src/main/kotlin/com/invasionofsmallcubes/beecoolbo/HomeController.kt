4c481d12858548c62815be53c23b96e16c4ad155
Emanuele Ianni <emanuele.ianni@lastminute.com>
2017-09-08 15:01:48 +0200

    added fe with react

---
+++ src/main/kotlin/com/invasionofsmallcubes/beecoolbo/HomeController.kt
@@ -0,0 +1,14 @@
+package com.invasionofsmallcubes.beecoolbo
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
+class HomeController {
+    @RequestMapping(value = "/")
+    fun index(): String {
+        return "index"
+    }
+
+}
--- src/test/kotlin/com/invasionofsmallcubes/beecoolbo/EmployeeIT.kt
+++ src/test/kotlin/com/invasionofsmallcubes/beecoolbo/EmployeeIT.kt
@@ -1,7 +1,6 @@
 package com.invasionofsmallcubes.beecoolbo
 
 import org.amshove.kluent.`should equal to`
-import org.amshove.kluent.shouldNotEqual
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
