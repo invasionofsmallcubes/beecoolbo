b1c8e041e510fc7f7372e2190fb34b32d6d78595
Emanuele Ianni <emanuele.ianni@lastminute.com>
2017-09-11 01:57:58 +0200

    added cookie

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, fetchUtils, Admin, Resource } from 'admin-on-rest';
 const ReactDOM = require('react-dom');
 
 import { PostList, PostEdit, PostCreate } from './employees';
 
+const httpClient = (url, options = {}) => {
+    options['credentials'] = 'include';
+    return fetchUtils.fetchJson(url, options);
+};
+
 const App = () => (
-        <Admin restClient={jsonServerRestClient('/api')}>
+        <Admin restClient={jsonServerRestClient('/api', httpClient)}>
 
         <Resource name="employees" list={PostList} edit={PostEdit} create={PostCreate} />
 
